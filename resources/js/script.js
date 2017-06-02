1e0f7f28c628b5aa5091c2be8856f3c7a505c9a8
Ben Logue <benjamin.logue73@gmail.com>
2017-06-02 11:36:49 -0700

    Create basic layout for project page.

--- resources/js/script.js
+++ resources/js/script.js
@@ -6,4 +6,8 @@ $(document).ready(function() {
      menu: '#menu',
      loopBottom: true
   });
+
+  $('#project1').addClass('animated bounceInLeft');
+  $('#project2').addClass('animated bounceInLeft');
+  $('#project3').addClass('animated bounceInLeft');
 });
